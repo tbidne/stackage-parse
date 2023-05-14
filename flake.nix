1286202e20b571c8a0a93409c46f1e67fbfe624f
Tommy Bidne <tbidne@protonmail.com>
2023-05-14 14:43:37 +1200

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -5,28 +5,21 @@
     flake = false;
   };
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.nix-hs-utils = {
+    url = "github:tbidne/nix-hs-utils";
+    inputs.flake-compat.follows = "flake-compat";
+  };
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs =
     inputs@{ flake-compat
     , flake-parts
+    , nix-hs-utils
     , nixpkgs
     , self
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       perSystem = { pkgs, ... }:
         let
-          buildTools = c: [
-            c.cabal-install
-            pkgs.zlib
-          ];
-          devTools = c: [
-            (hlib.dontCheck c.apply-refact)
-            (hlib.dontCheck c.cabal-fmt)
-            (hlib.dontCheck c.haskell-language-server)
-            (hlib.dontCheck c.hlint)
-            (hlib.dontCheck c.ormolu)
-            pkgs.nixpkgs-fmt
-          ];
           hlib = pkgs.haskell.lib;
           ghc-version = "ghc945";
           compiler = pkgs.haskell.packages."${ghc-version}".override {
@@ -36,50 +29,27 @@
             };
           };
           mkPkg = returnShellEnv:
-            compiler.developPackage {
-              inherit returnShellEnv;
+            nix-hs-utils.mkHaskellPkg {
+              inherit compiler pkgs returnShellEnv;
               name = "stackage-parse";
               root = ./.;
-              modifier = drv:
-                pkgs.haskell.lib.addBuildTools drv
-                  (buildTools compiler ++
-                    (if returnShellEnv then devTools compiler else [ ]));
             };
-          mkApp = drv: {
-            type = "app";
-            program = "${drv}/bin/${drv.name}";
-          };
+          hs-dirs = "app src test";
         in
         {
           packages.default = mkPkg false;
           devShells.default = mkPkg true;
 
           apps = {
-            format = mkApp (
-              pkgs.writeShellApplication {
-                name = "format";
-                text = builtins.readFile ./tools/format.sh;
-                runtimeInputs = [
-                  compiler.cabal-fmt
-                  compiler.ormolu
-                  pkgs.nixpkgs-fmt
-                ];
-              }
-            );
-            lint = mkApp (
-              pkgs.writeShellApplication {
-                name = "lint";
-                text = builtins.readFile ./tools/lint.sh;
-                runtimeInputs = [ compiler.hlint ];
-              }
-            );
-            lint-refactor = mkApp (
-              pkgs.writeShellApplication {
-                name = "lint-refactor";
-                text = builtins.readFile ./tools/lint-refactor.sh;
-                runtimeInputs = [ compiler.apply-refact compiler.hlint ];
-              }
-            );
+            format = nix-hs-utils.format {
+              inherit compiler hs-dirs pkgs;
+            };
+            lint = nix-hs-utils.lint {
+              inherit compiler hs-dirs pkgs;
+            };
+            lint-refactor = nix-hs-utils.lint-refactor {
+              inherit compiler hs-dirs pkgs;
+            };
           };
         };
       systems = [
