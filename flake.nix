de01e09f361bcd16c2b7f90378b9fff135e5b962
Tommy Bidne <tbidne@protonmail.com>
2024-05-24 14:44:04 +1200

    Update nix

--- flake.nix
+++ flake.nix
@@ -5,24 +5,29 @@
   inputs.nix-hs-utils.url = "github:tbidne/nix-hs-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs =
-    inputs@{ flake-parts
-    , nix-hs-utils
-    , nixpkgs
-    , self
+    inputs@{
+      flake-parts,
+      nix-hs-utils,
+      nixpkgs,
+      self,
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      perSystem = { pkgs, ... }:
+      perSystem =
+        { pkgs, ... }:
         let
           hlib = pkgs.haskell.lib;
-          ghc-version = "ghc963";
+          ghc-version = "ghc982";
           compiler = pkgs.haskell.packages."${ghc-version}";
-          mkPkg = returnShellEnv:
+          mkPkg =
+            returnShellEnv:
             nix-hs-utils.mkHaskellPkg {
               inherit compiler pkgs returnShellEnv;
               name = "stackage-parse";
               root = ./.;
             };
-          compilerPkgs = { inherit compiler pkgs; };
+          compilerPkgs = {
+            inherit compiler pkgs;
+          };
         in
         {
           packages.default = mkPkg false;
@@ -35,6 +40,8 @@
           };
         };
       systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
         "x86_64-darwin"
         "x86_64-linux"
       ];
